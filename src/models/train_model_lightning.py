902a06d0856f16a3a89408b46093a7881425d29a
granatb <bartek.granat@gmail.com>
2022-01-19 15:54:27 +0100

    added drifting robustness test

--- src/models/train_model_lightning.py
+++ src/models/train_model_lightning.py
@@ -5,6 +5,8 @@ from model_lightning import LightningModel
 from src.data import get_dataset
 import hydra
 from datetime import datetime
+import torchdrift
+import copy
 
 
 @hydra.main(config_name="training_conf.yaml", config_path="../../conf")
@@ -33,32 +35,34 @@ def main(cfg):
         ),
     )
 
-    # inputs, _ = next(iter(testloader))
-    # inputs_ood = corruption_function(inputs)
+    inputs, _ = next(iter(trainloader))
+    inputs_ood = corruption_function(inputs)
 
-    # N = 6
-    # model.eval()
-    # inps = torch.cat([inputs[:N], inputs_ood[:N]])
-    # model.cpu()
-    # # predictions = model.predict(inps).max(1).indices
+    N = 6
+    model.eval()
+    inps = torch.cat([inputs[:N], inputs_ood[:N]])
+    model.cpu()
+    # predictions = model.predict(inps).max(1).indices
 
-    # feature_extractor = copy.deepcopy(model)
-    # feature_extractor.classifier = torch.nn.Identity()
+    feature_extractor = copy.deepcopy(model)
+    feature_extractor.classifier = torch.nn.Identity()
 
-    # drift_detector = torchdrift.detectors.KernelMMDDriftDetector()
+    drift_detector = torchdrift.detectors.KernelMMDDriftDetector()
 
-    # torchdrift.utils.fit(trainloader, feature_extractor, drift_detector)
+    torchdrift.utils.fit(trainloader, feature_extractor, drift_detector)
 
-    # drift_detection_model = torch.nn.Sequential(
-    #     feature_extractor,
-    #     drift_detector
-    # )
+    drift_detection_model = torch.nn.Sequential(
+        feature_extractor,
+        drift_detector
+    )
 
-    # features = feature_extractor(inputs)
-    # score = drift_detector(features)
-    # p_val = drift_detector.compute_p_value(features)
-    # print(f'score: {score}, p_val: {p_val}')
+    features = feature_extractor(inputs)
+    score = drift_detector(features)
+    p_val = drift_detector.compute_p_value(features)
+    print(f'score: {score}, p_val: {p_val}')
 
+def corruption_function(x: torch.Tensor):
+    return torchdrift.data.functional.gaussian_blur(x, severity=2)
 
 if __name__ == "__main__":
     main()
